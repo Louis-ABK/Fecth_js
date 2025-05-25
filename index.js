18c10812090a0cb2f278ed8698e1e07249413959
Louis-ABK <biteguelouis@gmail.com>
2025-05-25 01:22:59 +0100

    rajout des commentaires et suppression du token

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ let image = document.getElementById("image");
 let numero = document.getElementById("numero"); 
 let name = document.getElementById("name");
 
-let changeUsers = async () => {
-    let number = Math.ceil(Math.random() * 45) +1;
-    
-    let urlAPI = `https://api.github.com/users/${number}`
+const changeUsers = async () => {
+    // Nombre aleatoire pour nos id qui nous aiderons pour notre url
+    let numberAl = Math.ceil(Math.random() * 45) + 1; //De 0 a 46 
 
-    let data = await fetch(urlAPI);
-    console.log(data);
+    // mise en place de notre url assosier a notre nombre aleatoire
+    let request = `https://api.github.com/users/${numberAl}`;
 
+    // execution de notre requete et stockage des informations dans notre variable data
+    let data = await fetch(request)
+    // console.log(data);
+
+    // trasformation de notre resultat de donnes en format Json
     let response = await data.json();
-    console.log(response);
+    // console.log(response);
 
+    // recuperation de donnees pour affichage
     image.src = response.avatar_url;
     numero.textContent = response.id;
     name.textContent = response.name;
-
 };
 
 changeUsers();
